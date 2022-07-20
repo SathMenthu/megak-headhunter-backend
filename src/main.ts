657d4c4444aa95b4445e77d1569b1415393e0c31
SathMenthu <33133742+SathMenthu@users.noreply.github.com>
2022-07-20 09:45:07 +0200

    Added cookie parser, rate limit, json rate limit, global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
+import * as cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.setGlobalPrefix('api');
+  app.use(json({ limit: '5mb' }));
+  app.use(cookieParser());
+  app.use(
+    rateLimit({
+      windowMs: 5 * 60 * 1000,
+      max: 100,
+    }),
+  );
   await app.listen(3004);
 }
 bootstrap();
