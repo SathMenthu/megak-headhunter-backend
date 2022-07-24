289f94a898e5328d0efd2706c478d3736de0a6d2
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2022-07-25 00:28:05 +0200

    Another findAll and findOne tests improvement. Refactored tests.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -6,9 +6,6 @@ import { User } from './entities/user.entity';
 describe('UserService', () => {
   let service: UserService;
 
-  jest.spyOn(User, 'find').mockResolvedValue([]);
-  jest.spyOn(User, 'findOneByOrFail').mockResolvedValue(new User());
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService, UtilitiesService],
@@ -21,23 +18,49 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return users list', async () => {
-    const result = {
-      isSuccess: true,
-      message: expect.any(String),
-      users: expect.any(Array),
-    };
+  describe('findAll', () => {
+    it('should handle error', async () => {
+      const result = {
+        isSuccess: false,
+        message: expect.any(String),
+      };
 
-    expect(await service.findAll()).toEqual(result);
+      expect(await service.findAll()).toEqual(result);
+    });
+
+    it('should return users list', async () => {
+      jest.spyOn(User, 'find').mockResolvedValue([]);
+
+      const result = {
+        isSuccess: true,
+        message: expect.any(String),
+        users: expect.any(Array),
+      };
+
+      expect(await service.findAll()).toEqual(result);
+    });
   });
 
-  it('should return user', async () => {
-    const result = {
-      isSuccess: true,
-      message: expect.any(String),
-      user: {},
-    };
+  describe('findOne', () => {
+    it('should handle error', async () => {
+      const result = {
+        isSuccess: false,
+        message: expect.any(String),
+      };
 
-    expect(await service.findOne('abc')).toEqual(result);
+      expect(await service.findOne('abc')).toEqual(result);
+    });
+
+    it('should return user', async () => {
+      jest.spyOn(User, 'findOneByOrFail').mockResolvedValue(new User());
+
+      const result = {
+        isSuccess: true,
+        message: expect.any(String),
+        user: {},
+      };
+
+      expect(await service.findOne('abc')).toEqual(result);
+    });
   });
 });
