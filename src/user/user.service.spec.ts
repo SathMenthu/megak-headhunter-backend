ebad1760d8bafbf93dcf5042c7ae36cef07cbb92
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2022-07-25 00:13:00 +0200

    Added findAll test.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { UtilitiesService } from '../utilities/utilities.service';
+import { User } from './entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
+
+  jest.spyOn(User, 'find').mockResolvedValue([]);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,5 +18,15 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return users list', async () => {
+    const result = {
+      isSuccess: expect.any(Boolean),
+      message: expect.any(String),
+      users: expect.any(Array),
+    };
+
+    expect(await service.findAll()).toEqual(result);
   });
 });
