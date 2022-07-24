0e8a571604312f39223aaaae9f8e0bf2d59ff3b4
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2022-07-25 01:26:25 +0200

    Added update test.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,9 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { UtilitiesService } from '../utilities/utilities.service';
 import { User } from './entities/user.entity';
+import { RolesEnum } from '../../types';
 
 describe('UserService', () => {
   let service: UserService;
+
+  const testUserObj = {
+    id: 'abc',
+    email: 'example@test.com',
+    password: 'test',
+    permissions: [RolesEnum.STUDENT],
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -61,6 +69,39 @@ describe('UserService', () => {
       };
 
       expect(await service.findOne('abc')).toEqual(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should handle error', async () => {
+      const result = {
+        isSuccess: false,
+        message: expect.any(String),
+      };
+
+      expect(await service.update('abc', testUserObj)).toEqual(result);
+    });
+
+    it('should update user data', async () => {
+      jest.spyOn(User.prototype, 'save').mockResolvedValue(new User());
+      jest.spyOn(User, 'findOneByOrFail').mockResolvedValue(new User());
+      jest.spyOn(service, 'userFilter').mockReturnValue({
+        id: 'abc',
+        email: 'example@test.com',
+        permissions: [RolesEnum.STUDENT],
+      });
+
+      const result = {
+        isSuccess: true,
+        message: expect.any(String),
+        user: {
+          id: expect.any(String),
+          email: expect.any(String),
+          permissions: [RolesEnum.STUDENT],
+        },
+      };
+
+      expect(await service.update('abc', testUserObj)).toEqual(result);
     });
   });
 });
