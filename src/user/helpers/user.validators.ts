d9b8d9befcd498c742e24c0901f15ee5625a6948
Iwo Matecki <iwo@iwomi.pl>
2022-08-09 11:44:34 +0200

    Change validation to BE and send validation info if something's wrong to userService.editUser method

--- src/user/helpers/user.validators.ts
+++ src/user/helpers/user.validators.ts
@@ -1,26 +1,50 @@
 import * as urlExists from 'url-exists';
+import fetch from 'node-fetch';
 import { StudentStatus } from '../../types/enums/student.status.enum';
 import { ExpectedContractTypeEnum, ExpectedTypeWorkEnum } from '../../types';
+import { User } from '../entities/user.entity';
 
 export const StudentStatusValidator = (studentStatus: StudentStatus) =>
   studentStatus in StudentStatus && studentStatus;
 
 export const ExpectedContractTypeValidator = (
   expectedContractType: ExpectedContractTypeEnum,
-) => expectedContractType in ExpectedContractTypeEnum && expectedContractType;
+) => {
+  if (
+    !(expectedContractType in ExpectedContractTypeEnum && expectedContractType)
+  ) {
+    throw new Error(
+      'Preferowany rodzaj umowy powinien zostać wybrany z opcji dostępnych na stronie.',
+    );
+  }
+  return expectedContractType;
+};
 
 export const ExpectedTypeWorkValidator = (
   expectedTypeWork: ExpectedTypeWorkEnum,
-) => expectedTypeWork in ExpectedTypeWorkEnum && expectedTypeWork;
+) => {
+  if (!(expectedTypeWork in ExpectedTypeWorkEnum && expectedTypeWork)) {
+    throw new Error(
+      'Preferowany tryb pracy powinien zostać wybrany z opcji dostępnych na stronie.',
+    );
+  }
+  return expectedTypeWork;
+};
 
-export const LinksValidator = (arrayOfLinks: Array<string> | string) => {
+export const LinksValidator = (
+  arrayOfLinks: Array<string> | string,
+  checkedValue: string,
+) => {
   if (typeof arrayOfLinks === 'string') {
     arrayOfLinks = arrayOfLinks.split(',');
   }
-  return (
+  if (
     arrayOfLinks &&
     arrayOfLinks.filter(async link => urlExists(link, (err, exists) => exists))
-  );
+  ) {
+    return arrayOfLinks;
+  }
+  throw new Error(`Pole ${checkedValue} zostało źle wypełnione`);
 };
 
 export const CityValidator = (city: string) =>
@@ -36,7 +60,104 @@ export const NumberInRangeValidator = (
   value: number,
   starts: number,
   ends: number,
-) => Number(value) && value >= starts && value <= ends && value;
+  checkedValue: string,
+) => {
+  if (
+    Number.isInteger(Number(value)) &&
+    Number(value) >= starts &&
+    Number(value) <= ends &&
+    value
+  ) {
+    return value;
+  }
+  throw new Error(
+    `Liczba w polu ${checkedValue} powinna zmieścić się w przedziale ${starts} do ${ends}.`,
+  );
+};
 
-export const BooleanValidator = (value: boolean | null | string) =>
-  typeof value === 'boolean' && value;
+export const BooleanValidator = (
+  value: boolean | null | string,
+  checkedValue: string,
+) => {
+  if (typeof value === 'boolean' && value) {
+    return value;
+  }
+  throw new Error(
+    `Wybór w polu ${checkedValue} powinien zostać wybrany z opcji dostępnych na stronie.`,
+  );
+};
+
+export const ValidateEmail = async (email, foundUserEmail, foundUserId) => {
+  if (!MailValidator(email)) {
+    throw new Error('To nie jest email.');
+  }
+  const checkUserByEmail = await User.findBy({ email });
+  if (
+    checkUserByEmail.length > 1 ||
+    checkUserByEmail[0].email !== foundUserEmail ||
+    checkUserByEmail[0].id !== foundUserId
+  ) {
+    throw new Error('Ten email jest już użyty w naszej bazie.');
+  }
+  return email;
+};
+
+export const StringValidator = (
+  string: string,
+  len: number,
+  nameOfField: string,
+) => {
+  if (string.length < len) {
+    throw new Error(`Minimalna długość pola ${nameOfField} wynosi ${len}.`);
+  }
+  return string;
+};
+
+export const GitHubUserNameValidator = async (userName: string) => {
+  const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+  if (!RegExp(regex).test(userName)) {
+    throw new Error(
+      'Niestety, podana wartość pola Nazwa na GitHub jest nieprawidłowa',
+    );
+  }
+  const { status } = await fetch(`https://github.com/${userName}/`);
+  if (status !== 200) {
+    throw new Error(
+      'Niestety, podana wartość pola Nazwa na GitHub jest nieprawidłowa. GitHub nie ma takiego użytkownika.',
+    );
+  }
+  return userName;
+};
+
+export const GitHubAvatarGetter = async (userName: string) => {
+  const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+  if (!RegExp(regex).test(userName)) {
+    throw new Error(
+      'Niestety, podana wartość pola Nazwa na GitHub jest nieprawidłowa',
+    );
+  }
+  const { status } = await fetch(`https://github.com/${userName}/`);
+  if (status !== 200) {
+    throw new Error(
+      'Niestety, podana wartość pola Nazwa na GitHub jest nieprawidłowa. GitHub nie ma takiego użytkownika.',
+    );
+  }
+  return userName;
+};
+export const PhoneNumberValidator = (
+  value: number,
+  starts: number,
+  ends: number,
+) => {
+  if (
+    Number.isInteger(Number(value)) &&
+    Number(value) >= starts &&
+    Number(value) <= ends &&
+    value
+  ) {
+    return value;
+  }
+  throw new Error(
+    'Numer telefonu jest zły. Powinien to być numer krajowy, mieć 9 cyfr bez numeru kierunkowego Polski i zera na początku. Pole może być puste.',
+  );
+};
